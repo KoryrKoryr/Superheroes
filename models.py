d917f7cdc427e915b9c063b3c014ac2db9f4ecb7
Denis Korir <denohrkoryr@gmail.com>
2024-10-07 19:36:43 +0300

    Modify Models to Use SQLAlchemy-Serializer

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin  # Import the SerializerMixin
 
 metadata = MetaData()
 
 db= SQLAlchemy(metadata=metadata)
 
 # Model representing the Hero
-class Hero(db.Model):
+class Hero(db.Model, SerializerMixin):
     __tablename__ = "heroes"
     
     id = db.Column(db.Integer, primary_key=True)
@@ -15,12 +16,15 @@ class Hero(db.Model):
 
     # One-to-many relationship with HeroPower
     hero_powers = db.relationship('HeroPower', backref='heroes', cascade='all, delete')
+
+    # Specify the fields you want to serialize
+    serialize_only = ("id", "name", "super_name")
     
     def __repr__(self):
         return f'<Hero id={self.id} name={self.name} super_name={self.super_name}>'
     
     # Model representing the Power
-class Power(db.Model):
+class Power(db.Model, SerializerMixin):
     __tablename__ = "powers" 
     
     id = db.Column(db.Integer, primary_key=True)
@@ -30,12 +34,14 @@ class Power(db.Model):
     # One-to-many relationship with HeroPower
     hero_powers = db.relationship('HeroPower', backref='powers', cascade='all, delete')
 
+    serialize_only = ("id", "name", "description")
+
     def __repr__(self):
         return f'<Power id={self.id} name={self.name} description={self.description}>'
 
 
 # Table representing the association between Hero and Power
-class HeroPower(db.Model):
+class HeroPower(db.Model, SerializerMixin):
     __tablename__= "hero_powers"
     
     id = db.Column(db.Integer, primary_key=True)
